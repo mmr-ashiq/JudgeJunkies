5925ade667c47bc293906fd0d0b241958efc01db
MD. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-01-08 13:00:13 +0600

    Create URI1009.cpp

---
+++ URI/URI1009.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double salary,sale;
+    char name[100];
+    cin>>name;
+    cin>>salary>>sale;
+    sale=salary+sale*.15;
+    cout<<"TOTAL = R$ "<<fixed<<setprecision(2)<<sale<<endl;
+
+    return 0;
+}
