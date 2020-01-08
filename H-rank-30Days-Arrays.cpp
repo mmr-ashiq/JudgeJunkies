5dab92680d350003748b2b2c0eb4a59a67dd28ff
MD. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-01-08 12:08:22 +0600

    Update H-rank-30Days-Arrays.cpp

--- H-rank-30Days-Arrays.cpp
+++ H-rank-30Days-Arrays.cpp
@@ -1,14 +1,17 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
     int n;
     int a[1000];
-    scanf("%d\n",&n);
+    cin>>n;
     for(int i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        cin>>a[i];
     }
     for(int i=n-1;i>=0;i--)
-        printf("%d ",a[i]);
+    {
+        cout<<a[i]<<" ";
+    }
     return 0;
 }
