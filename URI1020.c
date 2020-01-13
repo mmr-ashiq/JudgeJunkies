eaadaee29e1236ab65aff269a267538a617653f6
MD. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-01-13 15:53:19 +0600

    Create URI1020.c

---
+++ URI1020.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int days,final_days,final_month,final_year,for_days;
+    scanf("%d",&days);
+    final_year = days / 365;
+    final_month = days % 365 / 30;
+    for_days = ((days % 365) % 30);
+    final_days = for_days % 30;
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n",final_year,final_month,final_days);
+    return 0;
+}
