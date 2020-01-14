ad0a1de039c57af8697c9fc62db9792938628756
MD. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-01-14 17:13:29 +0600

    Create URI1043.c

---
+++ URI/URI1043.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,c,perimeter,area;
+    scanf("%f %f %f",&a,&b,&c);
+    if(a<(b+c) && b<(a+c) && c<(a+b))
+    {
+        perimeter=a+b+c;
+        printf("Perimetro = %.1f\n",perimeter);
+    }
+    else
+    {
+        area=((a+b)/2)*c;
+        printf("Area = %.1f\n",area);
+    }
+    return 0;
+}
