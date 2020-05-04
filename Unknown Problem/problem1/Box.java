43c00b33c956d1b7627aaa1e9a284d41396085e2
Md. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-05-04 13:36:28 +0600

    Create Box.java
    adding Box class into main class

---
+++ Unknown Problem/problem1/Box.java
@@ -0,0 +1,14 @@
+public class Box{
+  double height, width,depth;
+  
+  Box(double h,double w,double d){
+    height = h;
+    width = w;
+    depth = d;
+  }
+
+  void displayVol(){
+    double vol = height * width * depth;
+    System.out.println(vol);
+  }
+}
