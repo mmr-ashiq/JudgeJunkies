f75479666aa6a9bba1a5e9fb92c0562bfad11889
Md. Mizanur Rahman Ashiq <30866225+mmr-ashiq@users.noreply.github.com>
2020-10-11 01:37:50 +0600

    Create A.cpp

---
+++ Code Forces/Codeforces Round #675 (Div. 2)/A. Fence/A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+  
+int32_t main()
+{
+	c_p_c();
+	int t;
+	cin>>t;
+	while(t--){
+		int a,b,c;
+		cin>>a>>b>>c;
+		cout<<max(a,max(b,c))<<endl;
+	}
+	
+	return 0;
+}
